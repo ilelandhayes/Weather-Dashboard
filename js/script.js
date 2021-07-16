fff58f1ea9a4c40ae5688689f53a1eb3d2537053
Leland Hayes <lelandkhayes13@gmail.com>
2021-07-16 16:57:01 -0400

    updated all files, trying to get api to run right

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-const WEATHER_API_KEY = "f16dc1b2fb18e2b0f1b156400f1a084d";
+var WEATHER_API_KEY = "f16dc1b2fb18e2b0f1b156400f1a084d";
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid=f16dc1b2fb18e2b0f1b156400f1a084d';
+var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=city&daily.temp&daily.wind_speed&daily.humidity&daily.uvi&appid=f16dc1b2fb18e2b0f1b156400f1a084d';
 var cityInput = document.querySelector('#city-name');
 var searchButton = document.querySelector('#search-button');
 var currentDayCard = document.querySelector('#cd-content');
 var previousSearches = document.querySelector('.searchedCitiesContainer');
 
-function getApi(city) {
+// function that pulls weather api data
+function getApi() {
 
     fetch(requestUrl)
       .then(function (response) {
@@ -17,10 +18,9 @@ function getApi(city) {
     })
 };
 
-function searchHandler() {
-    searchButton.addEventListener("click", getApi)
-  };
-
+function cityInputs() {
+  cityInput.addEventListener('input')
+}
 
 function getLatestSearches() {
   var searchHistory = JSON.parse(localStorage.getItem("searches"));
@@ -49,10 +49,16 @@ function displayLatestSearches() {
   })
 }
 
-
-
 function loadSearchedCitys() {
     return;
+};
+
+
+function searchHandler() {
+
+  searchButton.addEventListener("click", getApi);
+
+
 };
 
 
