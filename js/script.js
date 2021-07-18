39fc2412bfae497fde855f26b509f030d3ff9d36
Leland Hayes <lelandkhayes13@gmail.com>
2021-07-18 15:52:39 -0400

    added functions to js for api call, updated HTML file

--- js/script.js
+++ js/script.js
@@ -1,65 +1,101 @@
-var WEATHER_API_KEY = "f16dc1b2fb18e2b0f1b156400f1a084d";
+const WEATHER_API_KEY = "f16dc1b2fb18e2b0f1b156400f1a084d";
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=city&daily.temp&daily.wind_speed&daily.humidity&daily.uvi&appid=f16dc1b2fb18e2b0f1b156400f1a084d';
 var cityInput = document.querySelector('#city-name');
 var searchButton = document.querySelector('#search-button');
 var currentDayCard = document.querySelector('#cd-content');
 var previousSearches = document.querySelector('.searchedCitiesContainer');
 
 // function that pulls weather api data
-function getApi() {
+function pullData(city) {
 
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);   
-    })
-};
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_API_KEY}`;
+  
+    var outCome = fetch(requestUrl)
+      .then(response => response.json())
+      .then(data => {
+        return data;
+      })
 
-function cityInputs() {
-  cityInput.addEventListener('input')
+      var latLong = {
+        latitude: outCome.coord.lat,
+        longitude: outCome.cord.lon
+      }
+
+      var oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLong.latitude}&lon=${latLong.longitude}&appid=${WEATHER_API_KEY}`
+
+      var oneCall = fetch(oneCallURL)
+          .then(response => response.json())
+          .then(data => {
+              return data;
+          })
+
+          var date = new Date(oneCall.current.dt * 1000);
+
+          var dateString = `(${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()})`;
+      
+          cityInput.textContent(outCome.name, dateString);
+      
+          return oneCall;
+}
+
+function loadRecentCitySearch () {
+
+    var recentTerms = getLatestSearches()
+
+    var recentSearch = recentTerms[0];
+
+
 }
 
 function getLatestSearches() {
+
   var searchHistory = JSON.parse(localStorage.getItem("searches"));
 
-  if (!searchHistory) {
-    searchHistory = ["Charlotte", "Chicago", "Los Angeles"];
+  if (searchHistory) {
+    searchHistory = ["Los Angeles"];
     localStorage.setItem("searches", JSON.stringify(searchHistory));
     return searchHistory;
   } else {
       return searchHistory;
   }
-};
+}
 
 // make button elements using the latest searches
 function displayLatestSearches() {
 
-  var searchHistory = getLatestSearches(); 
+  // grabs recent searches
+  var searchHistory = getLatestSearches();
+  
+  // clears container content
+  
 
-  searchHistory.forEach(searchString => {
+  for (var i = 0; i < 8; i++) {
     
-    var recentSearchButtonEl = document.createElement("button");
-    recentSearchButtonEl.classList.add("previousSearchButton");
-    recentSearchButtonEl.textContent = searchString;
+    if (searchHistory[i]) {
+    
+      // creating buttons withh recent searches
+      var recentSearchButtonEl = document.createElement("button");
+      recentSearchButtonEl.classList.add("previousSearchButton");
+      recentSearchButtonEl.textContent = searchHistory[i];
 
-    previousSearches.append(recentSearchButtonEl);
-  })
+      recentSearchButtonEl.addEventListener("click", pullData)
+
+      previousSearches.append(recentSearchButtonEl);
+  
+    }
+  }
 }
 
 function loadSearchedCitys() {
     return;
-};
+}
 
 
 function searchHandler() {
 
-  searchButton.addEventListener("click", getApi);
+  searchButton.addEventListener("click", pullData);
 
-
-};
+}
 
 
 function init() {
@@ -70,6 +106,6 @@ function init() {
 
     searchHandler();
 
-};
+}
 
 init();
